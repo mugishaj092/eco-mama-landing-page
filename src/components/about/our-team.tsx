a8577c3041d5242aa420a600f2188d923515750f
amiparadis250 <pishimweaime7@gmail.com>
2024-10-24 18:17:42 +0200

    fixing team project

--- src/components/about/our-team.tsx
+++ src/components/about/our-team.tsx
@@ -2,125 +2,119 @@ import { images } from '@/utils/constant/images'
 import Image from 'next/image'
 import React from 'react'
 
+
+
+const teamMembers = [
+    {
+        name: 'Joie Sophia UMUHOZA',
+        title: 'Chief Executive Officer of ECO-MAMA',
+        image: images.Joie,
+        
+    }
+]
 function OurTeam() {
     return (
-        <div className="bg-white w-full py-10 sm:py-10 flex flex-col">
-            <div className="w-full">
-                <div className="max-w-2xl">
+        <div className="bg-white w-full py-16">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="text-center mb-12">
                     <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                        Meet our <span className='text-primary'>Team</span>
+                        Meet our <span className="text-primary">Team</span>
                     </h2>
-                    <p className="mt-6 leading-2 text-gray-600 text-sm py-4">
-                        Libero fames augue nisl porttitor nisi, quis. Id ac elit odio vitae elementum enim vitae ullamcorper suspendisse.
+                    <p className="mt-4 text-gray-600 text-base max-w-2xl mx-auto">
+                        A team dedicated to empowering vulnerable women in Rwanda through AI-driven mental health support and telepsychiatry, addressing climate change's psychological impact
                     </p>
                 </div>
-                <ul className="flex flex-wrap gap-x-32 max-sm:gap-x-10 max-sm:gap-y-5 gap-y-10 justify-between">
-                    <li>
-                        <div className="flex items-center gap-x-6">
-                            <Image
-                                className="rounded-full h-16 w-16 object-cover"
-                                src={images.profile}
-                                alt="Profile Picture"
-                            />
-                            <div>
-                                <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
-                                    Leslie Alexander
-                                </h3>
-                                <p className="text-sm font-semibold leading-6 text-primary">
-                                    Co-Founder / CEO
-                                </p>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="flex items-center gap-x-6">
-                            <Image
-                                className="rounded-full h-16 w-16 object-cover"
-                                src={images.profile}
-                                alt="Profile Picture"
-                            />
-                            <div>
-                                <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
-                                    Leslie Alexander
-                                </h3>
-                                <p className="text-sm font-semibold leading-6 text-primary">
-                                    Co-Founder / CEO
-                                </p>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="flex items-center gap-x-6">
-                            <Image
-                                className="rounded-full h-16 w-16 object-cover"
-                                src={images.profile}
-                                alt="Profile Picture"
-                            />
-                            <div>
-                                <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
-                                    Leslie Alexander
-                                </h3>
-                                <p className="text-sm font-semibold leading-6 text-primary">
-                                    Co-Founder / CEO
-                                </p>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="flex items-center gap-x-6">
-                            <Image
-                                className="rounded-full h-16 w-16 object-cover"
-                                src={images.profile}
-                                alt="Profile Picture"
-                            />
-                            <div>
-                                <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
-                                    Leslie Alexander
-                                </h3>
-                                <p className="text-sm font-semibold leading-6 text-primary">
-                                    Co-Founder / CEO
-                                </p>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="flex items-center gap-x-6">
-                            <Image
-                                className="rounded-full h-16 w-16 object-cover"
-                                src={images.profile}
-                                alt="Profile Picture"
-                            />
-                            <div>
-                                <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
-                                    Leslie Alexander
-                                </h3>
-                                <p className="text-sm font-semibold leading-6 text-primary">
-                                    Co-Founder / CEO
-                                </p>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="flex items-center gap-x-6">
-                            <Image
-                                className="rounded-full h-16 w-16 object-cover"
-                                src={images.profile}
-                                alt="Profile Picture"
-                            />
-                            <div>
-                                <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
-                                    Leslie Alexander
-                                </h3>
-                                <p className="text-sm font-semibold leading-6 text-primary">
-                                    Co-Founder / CEO
-                                </p>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
+                
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-12">
+                <div className="flex flex-col items-center text-center">
+                        <Image
+                            className="rounded-full h-32 w-32 object-cover mb-4"
+                            src={images.Joie}
+                            alt="Joie Sophia UMUHOZA"
+                        />
+                        <h3 className="text-lg font-semibold text-gray-900">
+                            Joie Sophia UMUHOZA 
+                        </h3>
+                        <p className="text-primary font-medium mt-1">
+                        Founder,and ED of Eco-mama 
+                        </p>
+                    </div>
+                    <div className="flex flex-col items-center text-center">
+                        <Image
+                            className="rounded-full h-32 w-32 object-cover mb-4"
+                            src={images.Jospine}
+                            alt="Jospine Sonia UWIMENA"
+                        />
+                        <h3 className="text-lg font-semibold text-gray-900">
+                            Jospine Sonia UWIMENA
+                        </h3>
+                        <p className="text-primary font-medium mt-1">
+                        Registered midwife , women program lead
+                        </p>
+                    </div>
+                 
+
+                    <div className="flex flex-col items-center text-center">
+                        <Image
+                            className="rounded-full h-32 w-32 object-cover mb-4"
+                            src={images.jovise}
+                            alt="Jovise Solive UMUHIRE"
+                        />
+                        <h3 className="text-lg font-semibold text-gray-900">
+                            Jovise Solive UMUHIRE
+                        </h3>
+                        <p className="text-primary font-medium mt-1">
+                        Medical student, and COO of ECO-MAMA
+                        </p>
+                    </div>
+
+                    <div className="flex flex-col items-center text-center">
+                        <Image
+                            className="rounded-full h-32 w-32 object-cover mb-4"
+                            src={images.Paradis}
+                            alt="ISHIMWE Ami Paradis"
+                        />
+                        <h3 className="text-lg font-semibold text-gray-900">
+                            ISHIMWE Ami Paradis
+                        </h3>
+                        <p className="text-primary font-medium mt-1">
+                            Software Engineer
+                        </p>
+                    </div>
+
+                    <div className="flex flex-col items-center text-center">
+                        <Image
+                            className="rounded-full h-32 w-32 object-cover mb-4"
+                            src={images.Speciouse}
+                            alt="Speciose UZAMURANGA"
+                        />
+                        <h3 className="text-lg font-semibold text-gray-900">
+                            Speciose UZAMURANGA
+                        </h3>
+                        <p className="text-primary font-medium mt-1">
+                            Advisor, and community coordinator 
+                        </p>
+                    </div>
+
+                    
+
+                    <div className="flex flex-col items-center text-center">
+                        <Image
+                            className="rounded-full h-32 w-32 object-cover mb-4"
+                            src={images.Joseph}
+                            alt="MUGISHA Joseph"
+                        />
+                        <h3 className="text-lg font-semibold text-gray-900">
+                            MUGISHA Joseph
+                        </h3>
+                        <p className="text-primary font-medium mt-1">
+                            Software Engineer
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
-    );
+    )
 }
 
-export default OurTeam;
+export default OurTeam
--- src/utils/constant/images.ts
+++ src/utils/constant/images.ts
@@ -1,7 +1,20 @@
 import profile from '@/assets/images/jose-removebg-preview.png';
 import aboutShowcase from '@/assets/svg/aboutShowcase.svg'
+import jovise from '@/assets/images/Jovise_Solive_UMUHIRE-removebg-preview.png'
+import Jospine from '@/assets/images/Jospine_Sonia_UWIMENA-removebg-preview.png'
+import Speciouse from '@/assets/images/Speciose_UZAMURANGA-removebg-preview.png'
+import Joie from '@/assets/images/Joie_Sophia_UMUHOZA-removebg-preview.png'
+import Paradis from '@/assets/images/ami_paradis_1729695885785-removebg-preview.png'
+import Joseph from '@/assets/images/WhatsApp Image 2024-10-23 at 17.20.57_28f2cb61.jpg'
 
 export const images = {
     profile,
-    aboutShowcase
+    aboutShowcase,
+    jovise,
+    Jospine,
+    Speciouse,
+    Joie,
+    Paradis,
+    Joseph,
+    
 }
