e4a88fd5ab7bfd3f5a58570e02090bdfbe9fb099
amiparadis250 <pishimweaime7@gmail.com>
2024-10-25 16:13:48 +0200

    fixed women worrier programs

--- src/utils/constant/images.ts
+++ src/utils/constant/images.ts
@@ -4,8 +4,11 @@ import jovise from '@/assets/images/Jovise_Solive_UMUHIRE-removebg-preview.png'
 import Jospine from '@/assets/images/Jospine_Sonia_UWIMENA-removebg-preview.png'
 import Speciouse from '@/assets/images/Speciose_UZAMURANGA-removebg-preview.png'
 import Joie from '@/assets/images/Joie_Sophia_UMUHOZA-removebg-preview.png'
-import Paradis from '@/assets/images/ami_paradis_1729695885785-removebg-preview.png'
+import Paradis from '@/assets/images/ami_paradis_1729861537760-removebg-preview.png'
 import Joseph from '@/assets/images/WhatsApp Image 2024-10-23 at 17.20.57_28f2cb61.jpg'
+
+import farmer from "@/assets/images/farmer.png"
+import stormy from "@/assets/images/stormseeker-rX12B5uX7QM-unsplash.jpg"
 
 export const images = {
     profile,
@@ -16,5 +19,8 @@ export const images = {
     Joie,
     Paradis,
     Joseph,
+   
+    farmer,
+    stormy
+ }
     
-}
